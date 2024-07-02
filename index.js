d56e68b4be792e9e32da12726a4e6ed24d15d944
Matheus Migge <matheusmigge@gmail.com>
2024-07-02 11:59:13 -0300

    capitalizeFirstLetter function

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 alert('Bem vindo ao sistema super legal.')
 let nome = prompt('Pra começar, digite seu nome...')
+nome = capitalizeFirstLetter(nome)
 alert(`Olá, ${nome}. Massa ter você aqui conosco!`)
 let oQueQuer = prompt('Digite o que você deseja de mim hoje...')
-alert(`Sinto muito, ${nome}. Não consigo fazer isso...`)
+alert(`Sinto muito, ${nome}. Não consigo ${oQueQuer}...`)
